2aa7c96788666b1b7b6294520e4c302317277b91
Roy Sun <syzroy@gmail.com>
2025-06-24 00:52:34 +0100

    remove the need for environment specific layer setup

--- ffmpeg_layer.tf
+++ ffmpeg_layer.tf
@@ -2,7 +2,7 @@
 resource "aws_lambda_layer_version" "ffmpeg_layer" {
   s3_bucket                = aws_s3_bucket.ffmpeg_binaries.id
   s3_key                   = aws_s3_object.ffmpeg_layer_zip.key
-  layer_name               = "${local.project_name}-ffmpeg-layer-${var.environment}"
+  layer_name               = "${local.project_name}-ffmpeg-layer"
   description              = var.ffmpeg_layer_description
   compatible_architectures = ["x86_64"]
   source_code_hash         = aws_s3_object.ffmpeg_layer_zip.source_hash
